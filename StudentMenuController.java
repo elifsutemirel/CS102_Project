4d7f605c6de39ca6a67bdc48dd7c140a9191f371
elifDuman <eduman@ug.bilkent.edu.tr>
2024-07-18 20:02:39 +0300

    student menu controller created

---
+++ StudentMenuController.java
@@ -0,0 +1,54 @@
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.shape.Circle;
+import javafx.scene.text.Text;
+
+public class StudentMenuController {
+
+    @FXML
+    private Button chatStudent;
+
+    @FXML
+    private Text mailText;
+
+    @FXML
+    private Text onlineText;
+
+    @FXML
+    private Text priceText;
+
+    @FXML
+    private Circle profileStudent;
+
+    @FXML
+    private TextField searchBar;
+
+    @FXML
+    private Button sendMessageButton;
+
+    @FXML
+    private Button sendMessageButton2;
+
+    @FXML
+    private Button settingsStudent;
+
+    @FXML
+    private Text tutorFieldText;
+
+    @FXML
+    private Circle tutorProfile;
+
+    @FXML
+    private Circle tutorProfile2;
+
+    @FXML
+    private Text tutornameText;
+
+    @FXML
+    private Text usernameText;
+
+}
+
+
