d2270994b77ff04b1335e25830fd8ce6b344e5a4
elifsutemirel <su.temirel@ug.bilkent.edu.tr>
2024-07-18 21:30:47 +0300

    controllers again

---
+++ chatPageController.java
@@ -0,0 +1,25 @@
+public class chatPageController{
+    @FXML
+    private Button backButton;
+
+    @FXML
+    private ScrollPane chatScrollPane;
+
+    @FXML
+    private Circle generalProfileSign;
+
+    @FXML
+    private TextField messageTextField;
+
+    @FXML
+    private Ellipse profielBigCircle;
+
+    @FXML
+    private Circle profileLittleCircle;
+
+    @FXML
+    private Button sendMessageButton;
+
+    @FXML
+    private Text tutorNameText;
+}
---
+++ settingsController.java
@@ -0,0 +1,38 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+public class settingsController {
+
+    @FXML
+    private TextField biographyTexTfield;
+
+    @FXML
+    private Button logOutButton;
+
+    @FXML
+    private PasswordField newPasswordTextField;
+
+    @FXML
+    private TextField newUsernameTextField;
+
+    @FXML
+    private PasswordField oldPasswordTextField;
+
+    @FXML
+    private Button passwordSubmitButton;
+
+    @FXML
+    private ScrollPane reservedClassScrollPane;
+
+    @FXML
+    private Button usernameSubmitButton;
+
+    @FXML
+    private Text usernameText;
+
+}
+
